dc44e722e5b731a8d95b8fa76c2cb6341b6e5c18
xias <jack.xia.super@gmail.com>
2016-11-03 17:15:45 -0400

    activity survey add

---
+++ app/src/main/java/com/example/android/effectivenavigation/matching/SurveyActivity.java
@@ -0,0 +1,34 @@
+package com.example.android.effectivenavigation.matching;
+
+import android.os.Bundle;
+import android.app.Activity;
+import android.view.View;
+import android.widget.Button;
+import android.widget.SeekBar;
+
+import com.example.android.effectivenavigation.R;
+
+public class SurveyActivity extends Activity {
+    private int[] q_result = new int[7];
+    private Button match;
+    private SeekBar s1;
+
+
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_survey);
+
+
+
+
+
+
+
+
+
+    }
+
+}
