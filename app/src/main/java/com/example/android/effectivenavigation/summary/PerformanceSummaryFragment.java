cef5bc8e9f700aa37bd739c493f267c4fa6312a9
xias <jack.xia.super@gmail.com>
2016-11-24 18:58:05 -0500

    added performance summary

--- app/src/main/java/com/example/android/effectivenavigation/summary/PerformanceSummaryFragment.java
+++ app/src/main/java/com/example/android/effectivenavigation/summary/PerformanceSummaryFragment.java
@@ -8,6 +8,13 @@ import android.view.View;
 import android.view.ViewGroup;
 
 import com.example.android.effectivenavigation.R;
+import com.github.mikephil.charting.charts.LineChart;
+import com.github.mikephil.charting.data.Entry;
+import com.github.mikephil.charting.data.LineData;
+import com.github.mikephil.charting.data.LineDataSet;
+import com.github.mikephil.charting.utils.ColorTemplate;
+
+import java.util.ArrayList;
 
 
 /**
@@ -25,7 +32,37 @@ public class PerformanceSummaryFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_performance_summary, container, false);
+        View v = inflater.inflate(R.layout.fragment_performance_summary, container, false);
+        LineChart lineChart = (LineChart) v.findViewById(R.id.chart);
+
+
+        ArrayList<Entry> entries = new ArrayList<>();
+        entries.add(new Entry(4f, 0));
+        entries.add(new Entry(8f, 1));
+        entries.add(new Entry(6f, 2));
+        entries.add(new Entry(2f, 3));
+        entries.add(new Entry(18f, 4));
+        entries.add(new Entry(9f, 5));
+
+        LineDataSet dataset = new LineDataSet(entries, "# of Calls");
+
+        ArrayList<String> labels = new ArrayList<String>();
+        labels.add("January");
+        labels.add("February");
+        labels.add("March");
+        labels.add("April");
+        labels.add("May");
+        labels.add("June");
+
+        LineData data = new LineData(labels, dataset);
+        dataset.setColors(ColorTemplate.COLORFUL_COLORS); //
+        dataset.setDrawCubic(true);
+        dataset.setDrawFilled(true);
+
+        lineChart.setData(data);
+        lineChart.animateY(5000);
+
+        return v;
     }
 
 }
